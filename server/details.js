acb8cd2cfc8bc39b4b38a5ede75a4be0ef8664b5
zkj <438101845@qq.com>
2020-01-16 23:44:38 +0800

    [zkj]-#4-fix-adjust array question of the details.json

---
+++ server/details.js
@@ -0,0 +1,20 @@
+(function () {
+  const $movieTitle = document.getElementById("movie-title");
+  const $movieInfoImg = document.getElementById("movie-info-img");
+  const $infoContentGenres = document.getElementById("info-content-genres");
+  const $infoContentPubdates = document.getElementById("info-content-pubdates");
+  const $infoContentDurations = document.getElementById("info-content-durations");
+  const $infoContentScores = document.getElementById("info-content-scores");
+  const $moviePlot = document.getElementById("movie-plot");
+
+  //render details.html
+  function renderDetails(index) {
+    $movieTitle.innerHTML = detailsData[index].title + detailsData[index].original_title;
+    $movieInfoImg.setAttribute("src", detailsData[index].images.small);
+    $infoContentGenres.innerHTML = "类型: " + detailsData[index].genres[0];
+    $infoContentPubdates.innerHTML = "上映日期: " + detailsData[index].pubdates;
+    $infoContentDurations.innerHTML = "片长: " + detailsData[index].durations;
+    $infoContentScores.innerHTML = "豆瓣评分: " + detailsData[index].rating;
+    $moviePlot.innerHTML = detailsData[index].summary;
+  }
+})();
