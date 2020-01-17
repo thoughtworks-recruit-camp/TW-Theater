a0fb2c91d3288a3d28a695f5a55cd4432359d831
zkjxx1548 <438101845@qq.com>
2020-01-17 19:33:28 +0800

    [zkj]-#5-feat-finish results.js

--- client/results.js
+++ client/results.js
@@ -25,13 +25,33 @@ function renderResults(arr) {
   $main.innerHTML = `搜索 ${getLocationKeyword()}`;
   for (let i = 0; i < arr.length; i++) {
     $main.innerHTML += `<div class="search-div">`
-    + `<img src= alt="sorry" />`
+    + `<img src=${arr[i].images} alt="poster" />`
     + `<p class="search-info">`
-    + `<span class="info-title">美丽 (2008)</span>`
-    + `<span>评分: 6.3</span>`
-    + `<span>韩国 / 剧情 / Beautiful / 美丽 / 87分钟</span>`
+    + `<span class="info-title">${arr[i].title} (${arr[i].year})</span>`
+    + `<span>评分: ${arr[i].score}</span>`
+    + `<span>${updateArr(arr[i].genres)} / ${isCommonName(arr[i].title, arr[i].original_title)} / ${arr[i].durations[0]}</span>`
     + `<span>全宰洪 / 车秀妍 / 李天熙</span>`
+    + `${arr[i].summary}`
     + `</p>`
     + `</div>`
   }
 }
+
+function updateArr(arr) {
+  let res = "";
+  for (let i = 0; i < arr.length - 1; i++) {
+    res += arr[i] + " / ";
+  }
+  return res + arr[arr.length - 1];
+}
+
+function isCommonName(name1, name2) {
+  if (name1 === name2) {
+    return name1;
+  }
+  return name1 + " / " + name2;
+}
+
+window.onload = () => {
+  getResultsDataFromKeyword();
+};
